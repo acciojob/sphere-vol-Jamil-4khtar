052055d07bdf8534a3eb604168b9832683af167f
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-14 16:34:37 +0530

    formula

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 function volume_sphere() {
     //Write your code here
-	let radius = document.getElementById("radius").value;
-	let vol = 0
-	if (!(radius < 0)) {
-		vol = (4/3)*(Math.PI)*(Math.pow(radius, 3));
-	} else {
-		vol = "NaN"
-	}
-	let volume = document.getElementById("volume").value;
-	volume = vol;
-	
-  
+	let radius = document.getElementById("radius");
+	let volume = (4/3) * Math.PI * Math.pow(radius.value, 3);
+	document.getElementById("volume").value = volume.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
