9e15f24bfdad07f2c399a1713dd36767be191e6c
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-14 17:15:18 +0530

    Assignment complete

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function volume_sphere() {
     //Write your code here
 	let radius = document.getElementById("radius");
 	let volume = (4/3) * Math.PI * Math.pow(radius.value, 3);
-	document.getElementById("volume").value = volume.toFixed(2);
+	if(radius.value < 0 || isNaN(radius.value)) {
+		document.getElementById("volume").value = 'NaN';
+		return;
+	} else {
+		document.getElementById("volume").value = volume.toFixed(4);
+	}
+	
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = (e) => {
+	e.preventDefault();
+	volume_sphere();
+};
